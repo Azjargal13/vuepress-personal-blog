7e801785b3d44d2540b764b9eaedfeb699bacd7c
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2020-07-12 22:15:00 +0900

    add base style and an article

--- blog/.vuepress/enhanceApp.js
+++ blog/.vuepress/enhanceApp.js
@@ -4,7 +4,7 @@
 
 import VueKinesis from "vue-kinesis"
 import ImgLazy from 'vuepress-plugin-img-lazy/ImgLazy'
-
+import "../.vuepress/theme/styles/base.styl"
 export default ({
     Vue,
     options,
