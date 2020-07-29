369b31c01d980c38227449a2f85dd25b5770144d
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2020-07-29 19:28:16 +0900

    comment out kinesis elements

--- .vuepress/theme/layouts/Home.vue
+++ .vuepress/theme/layouts/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container home">
-    <kinesis-container :is="home">
-      <!-- <div class="grid grid-cols-3 gap-4 p-2">
+    <!-- <kinesis-container :is="home"> -->
+    <!-- <div class="grid grid-cols-3 gap-4 p-2">
         <kinesis-element :strength="15" type="depth" class="p-10">
           <img :src="codingSVG" alt class="p-2 h-24" />
           <span>Javascript/Typescript</span>
@@ -27,35 +27,35 @@
           <img :src="bookSVG" alt class="pa-2 h-24" />
           <span>bibliophile</span>
         </kinesis-element>
-      </div>-->
-      <div class="flex justify-center p-6">
-        <img :src="myImg" class="rounded-full w-48 border-gray-700 border-4 h-48 text-center p-0" />
-      </div>
-      <div class="flex justify-center">
-        <a href="https://www.linkedin.com/in/azjargal-gankhuyag/" target="_blank">
-          <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" class="h-20 ml-4 p-0" />
-        </a>
-        <a href="https://github.com/Azjargal13" target="_blank">
-          <img src="https://img.icons8.com/clouds/100/000000/github.png" class="h-20 ml-4 p-0" />
-        </a>
-        <a href="mailto:azjargalgankhuyag13@gmail.com" target="_blank">
-          <img src="https://img.icons8.com/clouds/100/000000/gmail.png" class="h-20 ml-4 p-0" />
-        </a>
-        <a href="/blog1/">
-          <img
-            src="https://img.icons8.com/clouds/100/000000/ball-point-pen.png"
-            class="h-20 ml-4 p-0"
-          />
-        </a>
-      </div>
-      <h4 class="text-medium font-thin text-center mt-4">Azjargal Gankhuyag</h4>
-      <h2 class="text-center p-10">
-        <kinesis-element :strength="1" type="scale" class="font-normal sm:text-medium">
-          Life motto:
-          <span v-for="m in motto" :key="m.id">{{m}}</span>
-        </kinesis-element>
-      </h2>
-    </kinesis-container>
+    </div>-->
+    <div class="flex justify-center p-6">
+      <img :src="myImg" class="rounded-full w-48 border-gray-700 border-4 h-48 text-center p-0" />
+    </div>
+    <div class="flex justify-center">
+      <a href="https://www.linkedin.com/in/azjargal-gankhuyag/" target="_blank">
+        <img src="https://img.icons8.com/clouds/100/000000/linkedin.png" class="h-20 ml-4 p-0" />
+      </a>
+      <a href="https://github.com/Azjargal13" target="_blank">
+        <img src="https://img.icons8.com/clouds/100/000000/github.png" class="h-20 ml-4 p-0" />
+      </a>
+      <a href="mailto:azjargalgankhuyag13@gmail.com" target="_blank">
+        <img src="https://img.icons8.com/clouds/100/000000/gmail.png" class="h-20 ml-4 p-0" />
+      </a>
+      <a href="/blog1/">
+        <img
+          src="https://img.icons8.com/clouds/100/000000/ball-point-pen.png"
+          class="h-20 ml-4 p-0"
+        />
+      </a>
+    </div>
+    <h4 class="text-medium font-thin text-center mt-4">Azjargal Gankhuyag</h4>
+    <h2 class="text-center p-10">
+      <!-- <kinesis-element :strength="1" type="scale" class="font-normal sm:text-medium">
+        Life motto:
+        <span v-for="m in motto" :key="m.id">{{m}}</span>
+      </kinesis-element> -->
+    </h2>
+    <!-- </kinesis-container> -->
   </div>
 </template> 
 
